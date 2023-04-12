be1299afe72c3dba6ebe5e58cdd8645e4cee48ae
JasonOktavian <jasonoktavian30@gmail.com>
2023-04-12 14:35:42 +0700

    Learning Blade Templating
    Still Learning about blade templating and how it helps programmer to code in laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,64 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Jason Oktavian",
+        "email" => "jasonoktavian30@gmail.com",
+        "image" => "MHA.png",
+        "title" => "About"
+    ]);
+});
+
+Route::get('/blog', function () {
+    $blog_post = [
+        [
+            "title" => "Diary Of A Wimpy Kid",
+            "slug" => "diary-of-a-wimpy-kid",
+            "author" => "Jason Oktavian",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
+        ],
+        [
+            "title" => "Marvel End Game",
+            "slug" => "marvel-end-game",
+            "author" => "Jason Oktavian",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_post
+    ]);
+});
+Route::get('posts/{slug}', function ($slug) {
+    $blog_post = [
+        [
+            "title" => "Diary Of A Wimpy Kid",
+            "slug" => "diary-of-a-wimpy-kid",
+            "author" => "Jason Oktavian",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
+        ],
+        [
+            "title" => "Marvel End Game",
+            "slug" => "marvel-end-game",
+            "author" => "Jason Oktavian",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
+        ],
+    ];
+    $new_post = [];
+    foreach ($blog_post as $post) {
+        if ($post["slug"] == $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+
+    ]);
 });
