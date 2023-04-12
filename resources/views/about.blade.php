61e66796793339c2e3ac6a4f5c67155c0434e46a
JasonOktavian <jasonoktavian30@gmail.com>
2023-04-12 16:18:46 +0700

    Collection and Controller
    Learning about simple collection and controller without the use of a database and still have static variables

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
 @section('container')
     <h1>Halaman About</h1>
-    <h3>{{$name}}</h3>
+    <h3>{{ $name }}</h3>
     <h4>{{ $email }}</h4>
-    <img src="img/{{ $image }}" alt="{{ $name }}" width="30%">
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="30%" class="img-thumbnail">
 @endsection
-    
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,50 +32,6 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_post = [
-        [
-            "title" => "Diary Of A Wimpy Kid",
-            "slug" => "diary-of-a-wimpy-kid",
-            "author" => "Jason Oktavian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
-        ],
-        [
-            "title" => "Marvel End Game",
-            "slug" => "marvel-end-game",
-            "author" => "Jason Oktavian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
-        ],
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_post
-    ]);
-});
-Route::get('posts/{slug}', function ($slug) {
-    $blog_post = [
-        [
-            "title" => "Diary Of A Wimpy Kid",
-            "slug" => "diary-of-a-wimpy-kid",
-            "author" => "Jason Oktavian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
-        ],
-        [
-            "title" => "Marvel End Game",
-            "slug" => "marvel-end-game",
-            "author" => "Jason Oktavian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
-        ],
-    ];
-    $new_post = [];
-    foreach ($blog_post as $post) {
-        if ($post["slug"] == $slug) {
-            $new_post = $post;
-        }
-    }
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
+Route::get('/blog', [PostController::class, 'index']);
 
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
