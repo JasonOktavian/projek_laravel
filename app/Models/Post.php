7f0af5f4ed52508b6eeaac551875e7fb37680b60
JasonOktavian <jasonoktavian30@gmail.com>
2023-04-17 16:13:41 +0700

    Post Model
    Membuat post model dimana model yang lama sudah tidak digunakan dan menggantinya dengan model yang sudah menggunakan php artisan serta route model binding menggunakan slug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-// class Post extends Model
-// {
-//     use HasFactory;
-// }
-
-class Post
+class Post extends Model
 {
-    private static $blog_post = [
-        [
-            "title" => "Diary Of A Wimpy Kid",
-            "slug" => "diary-of-a-wimpy-kid",
-            "author" => "Jason Oktavian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
-        ],
-        [
-            "title" => "Marvel End Game",
-            "slug" => "marvel-end-game",
-            "author" => "Jason Oktavian",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem deserunt nostrum consequatur incidunt! Debitis soluta veritatis at totam! Veniam dolores alias sint possimus ex velit error in voluptate a quia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore et est praesentium sequi eos vel quas iusto eius eligendi placeat fugit, deserunt quae delectus perferendis cumque vitae facere nisi ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam labore atque fuga. Ipsum rerum veniam possimus esse at et modi maxime, quam tenetur aliquam inventore nostrum vel dolorum aut necessitatibus."
-        ],
-    ];
-    public static function all()
-    {
-        return collect(self::$blog_post);
-    }
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    use HasFactory;
+    //Yang diperbolehkan Mass Insert
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    //Yang tidak diperbolehkan mass insert
+    protected $guarded = ['id'];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,14 +5,25 @@
 
 <article class="mb-5">
     <h2>
-        {{ $post["title"] }}
+        {{ $post->title }}
     </h2>
-    <h5>{{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    {{-- <h5>{{ $post["author"] }}</h5> --}}
+    {{-- {{ $post->body }} --}}
+    {!! $post->body !!}
 </article>
 
 <a href="/blog">Back</a>
 
 @endsection
 
+
+
+
+
+{{-- Post::create([
+    'title' => 'Third Blog',
+    'slug'=>'third-blog',
+    'excerpt'=> 'Third lorem ipsum excerpt',
+    'body'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quos odio laudantium sint soluta consequatur ea culpa maiores accusamus? Esse dolorem, atque provident assumenda, velit, repellat ipsa error voluptatem iusto nemo et! Dicta magni nisi neque! Ipsa blanditiis dolorum accusantium sapiente, possimus fugit numquam? Saepe molestias repellendus error! Ducimus quisquam repudiandae molestiae incidunt.</p><p>Tempora expedita saepe minima illo debitis tenetur maxime quia quo. Hic pariatur saepe excepturi temporibus, numquam dignissimos eligendi animi unde perferendis quod? Enim itaque expedita, neque praesentium, alias perspiciatis beatae ut vero fugiat eius aliquid temporibus a ea exercitationem quibusdam molestiae hic nisi, deleniti commodi molestias! Minima similique obcaecati dicta sed, aliquam minus sequi, eos corporis debitis quidem commodi fugit cumque, tempore nesciunt.</p><p>Facilis, nulla veniam ipsum totam eum optio, velit cupiditate quos, molestias doloribus asperiores cum debitis laboriosam nisi amet sit assumenda vero! Debitis officiis beatae velit sequi vero consequuntur a, dolor nisi qui quae corporis autem omnis quisquam possimus. Laborum, magni.</p><p>Enim quibusdam maxime voluptate quo itaque velit debitis molestias deleniti vel temporibus, assumenda distinctio ullam cupiditate? Delectus quod officia eos ipsum possimus necessitatibus eius corporis, ducimus nihil recusandae vitae quibusdam? Atque vero, alias recusandae, id ipsam, laudantium dicta nulla necessitatibus commodi dolorem saepe sequi et aperiam ipsa labore asperiores reiciendis accusantium illum similique. Consequatur mollitia, cum debitis nobis doloremque ad necessitatibus expedita qui! Ad neque aut quasi cupiditate in delectus illo molestias debitis libero officia id voluptates eum dignissimos porro animi, similique necessitatibus! Vero natus provident animi doloribus illum labore quis enim mollitia quasi?</p>'
+]) --}}
 
