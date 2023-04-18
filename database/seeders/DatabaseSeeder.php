3be811c08798c8a78fc0eba93b8d3dcd54dbafde
JasonOktavian <jasonoktavian30@gmail.com>
2023-04-18 15:15:23 +0700

    Seeder
    Using Seeder for database without using factory so the data is still manually inserted

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,59 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'name' => 'Jason Oktavian',
+            'email' => 'jasonoktavian30@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        User::create([
+            'name' => 'Anthony Chandra',
+            'email' => 'anthony.chandra@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        Post::create([
+            'title' => 'First Blog',
+            'category_id' => 1,
+            'user_id' => 1,
+            'slug' => 'first-blog',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ex, vel perspiciatis nemo vero corporis repellendus quibusdam rem est veritatis?',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, beatae. Doloribus ipsa cum nemo, hic sed aperiam ipsam harum consequuntur unde non voluptas quo! Dolores magnam voluptatum commodi inventore, facilis hic explicabo illo doloremque iste consectetur velit esse aspernatur enim optio placeat.</p> <p>Maiores quisquam temporibus similique suscipit illo, amet nihil pariatur sunt iusto saepe, perferendis, repudiandae dicta magnam voluptas consequatur.</p> <p>Omnis earum vitae necessitatibus eos minima similique dicta. Similique corrupti tenetur, repellendus officia neque nostrum fuga reprehenderit repudiandae quod fugiat expedita optio cum saepe quasi voluptatem delectus incidunt quisquam. Recusandae fugiat doloribus, odit laudantium perferendis harum corporis cumque reiciendis laborum!</p>'
+
+        ]);
+        Post::create([
+            'title' => 'Second Blog',
+            'category_id' => 1,
+            'user_id' => 2,
+            'slug' => 'second-blog',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ex, vel perspiciatis nemo vero corporis repellendus quibusdam rem est veritatis?',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, beatae. Doloribus ipsa cum nemo, hic sed aperiam ipsam harum consequuntur unde non voluptas quo! Dolores magnam voluptatum commodi inventore, facilis hic explicabo illo doloremque iste consectetur velit esse aspernatur enim optio placeat.</p> <p>Maiores quisquam temporibus similique suscipit illo, amet nihil pariatur sunt iusto saepe, perferendis, repudiandae dicta magnam voluptas consequatur.</p> <p>Omnis earum vitae necessitatibus eos minima similique dicta. Similique corrupti tenetur, repellendus officia neque nostrum fuga reprehenderit repudiandae quod fugiat expedita optio cum saepe quasi voluptatem delectus incidunt quisquam. Recusandae fugiat doloribus, odit laudantium perferendis harum corporis cumque reiciendis laborum!</p>'
+
+        ]);
+        Post::create([
+            'title' => 'Third Blog',
+            'category_id' => 2,
+            'user_id' => 1,
+            'slug' => 'third-blog',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ex, vel perspiciatis nemo vero corporis repellendus quibusdam rem est veritatis?',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, beatae. Doloribus ipsa cum nemo, hic sed aperiam ipsam harum consequuntur unde non voluptas quo! Dolores magnam voluptatum commodi inventore, facilis hic explicabo illo doloremque iste consectetur velit esse aspernatur enim optio placeat.</p> <p>Maiores quisquam temporibus similique suscipit illo, amet nihil pariatur sunt iusto saepe, perferendis, repudiandae dicta magnam voluptas consequatur.</p> <p>Omnis earum vitae necessitatibus eos minima similique dicta. Similique corrupti tenetur, repellendus officia neque nostrum fuga reprehenderit repudiandae quod fugiat expedita optio cum saepe quasi voluptatem delectus incidunt quisquam. Recusandae fugiat doloribus, odit laudantium perferendis harum corporis cumque reiciendis laborum!</p>'
+
+        ]);
+        Post::create([
+            'title' => 'Fourth Blog',
+            'category_id' => 2,
+            'user_id' => 2,
+            'slug' => 'fourth-blog',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum ex, vel perspiciatis nemo vero corporis repellendus quibusdam rem est veritatis?',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, beatae. Doloribus ipsa cum nemo, hic sed aperiam ipsam harum consequuntur unde non voluptas quo! Dolores magnam voluptatum commodi inventore, facilis hic explicabo illo doloremque iste consectetur velit esse aspernatur enim optio placeat.</p> <p>Maiores quisquam temporibus similique suscipit illo, amet nihil pariatur sunt iusto saepe, perferendis, repudiandae dicta magnam voluptas consequatur.</p> <p>Omnis earum vitae necessitatibus eos minima similique dicta. Similique corrupti tenetur, repellendus officia neque nostrum fuga reprehenderit repudiandae quod fugiat expedita optio cum saepe quasi voluptatem delectus incidunt quisquam. Recusandae fugiat doloribus, odit laudantium perferendis harum corporis cumque reiciendis laborum!</p>'
+
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,14 +8,14 @@
         {{ $post->title }}
     </h2>
 
-    <p>By. Jason Oktavian in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></p>
 
     {{-- <h5>{{ $post["author"] }}</h5> --}}
     {{-- {{ $post->body }} --}}
     {!! $post->body !!}
 </article>
 
-<a href="/blog">Back</a>
+<a href="/blog"class="text-decoration-none">Back</a>
 
 @endsection
 
